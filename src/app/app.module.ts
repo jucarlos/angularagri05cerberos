b390f6118af27a95eabb2feb9a64a6e31157357c
Juan Carlos <jcarlosjccm@gmail.com>
2022-10-21 12:48:23 +0200

    App terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from './auth/keycloak-init.factory';
+import { ConfigInitService } from './auth/config-init.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    KeycloakAngularModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [ KeycloakService, ConfigInitService ]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
